ae045f264fb1677b67662089df9265e292d6c56a
UnnatiUmesh <unnatiuk.2003@gmail.com>
2025-04-18 17:51:40 +0530

    AddHeroBlock.tsx file

---
+++ app/blocks/hero-block/HeroBlock.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import HeroText from './hero-text/HeroText'
+import HeroImage from './hero-image/HeroImage'
+
+
+const HeroBlock = () => {
+  return (
+    <div className='container mx-auto'>
+        <div className='flex flex-row justify-evenly items-center mt-20 ml-20'>
+            <div className='w-1/2'>
+            <HeroText />
+            </div>
+            <div className='w-1/2'>
+         <HeroImage />
+         </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroBlock
