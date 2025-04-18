32337d845c7333075ed35960d297c0aa26c91402
UnnatiUmesh <unnatiuk.2003@gmail.com>
2025-04-18 17:50:03 +0530

    Include AboutText.tsx file

---
+++ app/blocks/about-block/about-text/AboutText.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const AboutText = () => {
+  return (
+    <div className="max-w-6xl mx-auto px-6">
+      <div className="grid gap-12 text-justify">
+        <div>
+          <h3 className="text-2xl font-bold mb-4">Know About Me!</h3>
+          <p className="mb-4">
+            Hey there! I'm Unnati Umesh Kulkarni, <strong>a passionate Computer Science student</strong> from India with a strong interest in problem-solving, software development, and web technologies.
+          </p>
+          <p className="mb-4">
+            I'm actively building skills in <strong>HTML, CSS, JavaScript, React.js, and Next.js</strong>, and I enjoy turning ideas into responsive, user-friendly web experiences. I'm also exploring backend technologies and aiming to grow as a full-stack developer.
+          </p>
+          <p className="mb-4">
+            I love tackling algorithmic challenges and constantly seek to deepen my understanding of <strong >data structures, algorithms, and software architecture</strong>. Whether it's debugging tricky bugs or designing a clean UI, I'm always up for the challenge.
+          </p>
+          <p>
+            I'm currently focused on learning, growing, and contributing to real-world projects that push my skills and creativity forward. Every project is a step closer to becoming the developer I aspire to be.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutText;
